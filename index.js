e0eaedfbb87ae0ed840f4776d688ffd9ee59c4a4
Arseniy22877 <102078328+Arseniy22877@users.noreply.github.com>
2022-11-26 15:39:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {Partials} = require('discord.js');
 const message = require('./models/message');
 
 const channel_id = "1043830298732933150";
-const TOKEN = "OTk2MDc5NjQxODY3NjA0MDA4.Guk7ME.YhgAYu_Irrm-YsCmv75zLMkIwfMv30_kcyWbO4";
+const TOKEN = "OTk2MDc5NjQxODY3NjA0MDA4.GO6mpo.jVa4avn4lvfoyg7Tw_58qb3yp-oL3Uq1DzsRKY";
 const banned_channel_id = "1043820095966814230";
 const approved_channel_id = "1043820063125413888";
 
